b23706b390a2198d44ab37f4d11a769d65462cad
jvx <108929149+jvxis@users.noreply.github.com>
2023-12-11 13:26:07 -0300

    Update sats4plus.py
    This script sells some info about your node every day, channels, and their capacity, and you get some SATs back as payment for this info.

--- sats4plus.py
+++ sats4plus.py
@@ -4,7 +4,7 @@ from datetime import datetime, timedelta
 import time
 
 #Dependencies
-
+#You need to create an account for your node on https://sparkeer.space
 
 #Replace with your API_KEY - you can get it on https://sparkseer.space/account
 API_KEY = "SPARKEER_API_KEY"
