84537cf6b3eca3f565bf844b539c764847a01626
jvx <108929149+jvxis@users.noreply.github.com>
2023-12-13 07:03:28 -0300

    Update openchannel-pro.py
    Add function to connect with peer

--- openchannel-pro.py
+++ openchannel-pro.py
@@ -1,6 +1,21 @@
 #Dependencies: Box of Satoshis
 import subprocess
 
+#Replace with your full path to Umbrel directory.
+path_to_umbrel = "Your_Path_TO_Umbrel"
+
+def connect_to_node(node_key_address):
+    command = f"{path_to_umbrel}/scripts/app compose lightning exec lnd lncli connect {node_key_address}"
+    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    output, error = process.communicate()
+    output = output.decode("utf-8")
+
+    if process.returncode == 0:
+        print(f"Successfully connected to node {node_key_address}")
+    else:
+        print(f"Error connecting to node {node_key_address}: {error}")
+
+
 def get_utxos():
     command = "bos utxos"
     process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -54,8 +69,11 @@ def calculate_utxos_required_and_fees(amount_input, fee_per_vbyte):
     return utxos_needed, fee_cost, related_outpoints if related_outpoints else None
 
 # Example usage:
+node_key_address= input("Informe o public key e host do Node (formato pubkey@host):")
 input_amount = float(input("Digite o valor desejado para transacionar em satoshis: "))
 fee_per_vbyte = float(input("\nInsira a fee on-chain em sat/vByte: "))
+
+node_pub_key, node_host = node_key_address.split('@', 1)
 
 utxos_needed, onchain_fee_cost, all_outpoints = calculate_utxos_required_and_fees(input_amount, fee_per_vbyte)
 
@@ -64,8 +82,11 @@ if utxos_needed >= 0:
     formatted_onchain_fee_cost = f"{onchain_fee_cost:,.0f}".replace(",", ".")
     print(f"\nSão necessárias {utxos_needed} UTXOs para transferir {formatted_input_amount} satoshis.")
     print(f"A fee on-chain necessária para a transação é: {formatted_onchain_fee_cost} satoshis")
+    print(f"All outpoints related to the utxos considered: {all_outpoints}")
+    print(f"Connecting with peer: {node_pub_key}")
+    connect_to_node(node_key_address)
     formatted_outpoints = ' '.join([f'--utxo {outpoint}' for outpoint in all_outpoints])
-    print(f"\nComando de abertura do canal:\n/path/to/umbrel/scripts/app compose lightning exec lnd lncli openchannel --node_key <node_public_key> --sat_per_vbyte={int(fee_per_vbyte)} {formatted_outpoints} --local_amt={int(input_amount)}")
+    print(f"\nComando de abertura do canal:\n{path_to_umbrel}/scripts/app compose lightning exec lnd lncli openchannel --node_key {node_pub_key} --sat_per_vbyte={int(fee_per_vbyte)} {formatted_outpoints} --local_amt={int(input_amount)}")
 
 else:
     print("Não há UTXOs suficientes.")
